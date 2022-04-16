c9e0f1ea0f98f8f2ff575d7682c40d75e0a7e6d0
Fatih Kayan <fatih.kayan83@gmail.com>
2022-04-16 16:38:55 +0300

    feat: add layout for pages

---
+++ web/pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import styles from "@styles/home/Home.module.scss";
+import { Navbar } from "@components/layout/Navbar";
+
+const HomePage = () => {
+  return (
+    <div>
+      <div>asdfasdf</div>
+    </div>
+  );
+};
+
+export default HomePage;
